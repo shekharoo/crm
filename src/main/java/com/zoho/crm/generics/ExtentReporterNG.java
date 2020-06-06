78cbf45fd04d3fdab577b3a83f071e27c3dfc08e
shekharoo <shekuemail@gmail.com>
2020-06-06 12:02:10 +0530

    Zoho Update Files

--- src/main/java/com/zoho/crm/generics/ExtentReporterNG.java
+++ src/main/java/com/zoho/crm/generics/ExtentReporterNG.java
@@ -2,6 +2,7 @@ package com.zoho.crm.generics;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class ExtentReporterNG {
 	public static ExtentReports extent;
@@ -9,12 +10,16 @@ public class ExtentReporterNG {
 	{
 		String path = System.getProperty("user.dir")+"\\reports\\index.html";
 		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setTheme(Theme.STANDARD);
+		reporter.config().setEncoding("utf-8");
 		reporter.config().setReportName("Zoho Crm Automation Results");
 		reporter.config().setDocumentTitle("Test Results");
+		reporter.config().setAutoCreateRelativePathMedia(true);
 		
 		extent = new ExtentReports();
-		extent.attachReporter(reporter);
 		extent.setSystemInfo("Tester", "Shekhar Anand");
+		extent.attachReporter(reporter);
+		
 		
 		return extent;
 	}
